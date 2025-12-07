1cf059db7d82b292392c588a45a201472de45ba9
hollowillow <slavicekmichal@proton.me>
2025-12-07 21:52:47 +0100

    Fix KubeJS and addons compat issues after Fabric 1.20.1 transition

--- kubejs/server_scripts/lootShulkers.js
+++ kubejs/server_scripts/lootShulkers.js
@@ -1,6 +1,6 @@
-LootJS.lootTables(event => {
-        event.getEntityTable("minecraft:shulker")
-                .clear()
-                .createPool()
-                .addEntry("2x minecraft:shulker_shell");
-})
+LootJS.modifiers((event) => {
+    event
+        .addEntityLootModifier("minecraft:shulker")
+        .removeLoot("minecraft:shulker_shell")
+        .addLoot("2x minecraft:shulker_shell");
+});
--- kubejs/server_scripts/recipeViewerEvents.js
+++
@@ -1,4 +0,0 @@
-// remove duplicate xp buckets from sophisticated core and enchantment industries
-RecipeViewerEvents.removeEntries('item', event => {
-	event.remove('sophisticatedcore:xp_bucket')
-})
